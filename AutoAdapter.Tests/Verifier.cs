8b742ce6c6cab419da0cf3bb9aac3c9f053cc762
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-03 23:03:45 +0300

    Making sure tests fail if PEVerify.exe is not found. Also adding more possible locations of PEVerify.exe

--- AutoAdapter.Tests/Verifier.cs
+++ AutoAdapter.Tests/Verifier.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
+using AutoAdapter.Fody;
 using NUnit.Framework;
 
 namespace AutoAdapter.Tests
@@ -16,14 +17,14 @@ namespace AutoAdapter.Tests
             Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
         }
 
-        static string Validate(string assemblyPath2)
+        private static string Validate(string assemblyPath)
         {
-            var exePath = GetPathToPEVerify();
-            if (!File.Exists(exePath))
-            {
-                return string.Empty;
-            }
-            var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
+            var exePath = GetPathToPeVerify();
+
+            if (exePath.HasNoValue)
+                throw new Exception("Could not find PEVerify.exe");
+
+            var process = Process.Start(new ProcessStartInfo(exePath.GetValue(), "\"" + assemblyPath + "\"")
             {
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
@@ -31,21 +32,27 @@ namespace AutoAdapter.Tests
             });
 
             process.WaitForExit(10000);
-            return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+
+            return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath, "");
         }
 
-        static string GetPathToPEVerify()
+        private static Maybe<string> GetPathToPeVerify()
         {
-            var exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
+            var programFilesX86 = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
 
-            if (!File.Exists(exePath))
+            var possiblePathds = new[]
             {
-                exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
-            }
-            return exePath;
+                $@"{programFilesX86}\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\PEVerify.exe",
+                $@"{programFilesX86}\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\PEVerify.exe",
+                $@"{programFilesX86}\Microsoft SDKs\Windows\v8.1A\Bin\NETFX 4.5.1 Tools\PEVerify.exe",
+                $@"{programFilesX86}\Microsoft SDKs\Windows\v10.0A\Bin\NETFX 4.6 Tools\PEVerify.exe",
+                $@"{programFilesX86}\Microsoft SDKs\Windows\v10.0A\Bin\NETFX 4.6.1 Tools\PEVerify.exe"
+            };
+
+            return possiblePathds.FirstOrNoValue(File.Exists);
         }
 
-        static string TrimLineNumbers(string foo)
+        private static string TrimLineNumbers(string foo)
         {
             return Regex.Replace(foo, @"0x.*]", "");
         }

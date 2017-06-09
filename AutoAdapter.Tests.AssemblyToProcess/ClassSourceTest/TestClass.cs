1ff61456a5447111df5d0e4be8a94917ca6d4395
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-10 00:12:39 +0300

    Making sure that AutoAdapter supports the case when the source type is a class, not an interface.

---
+++ AutoAdapter.Tests.AssemblyToProcess/ClassSourceTest/TestClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.ClassSourceTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TSource, TDestination>(TSource instance)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<SourceClass, IDestinationInterface>(new SourceClass());
+
+            adapter.Echo("Input").Should().Be("Input");
+        }
+    }
+
+    public interface IDestinationInterface
+    {
+        string Echo(string value);
+    }
+
+    public class SourceClass
+    {
+        public string Echo(string value)
+        {
+            return value;
+        }
+    }
+}

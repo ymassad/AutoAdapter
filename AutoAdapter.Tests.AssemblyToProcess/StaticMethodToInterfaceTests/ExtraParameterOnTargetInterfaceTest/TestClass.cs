600b6bc2a27f2fef8cd586a7a2220e94d364e19e
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-24 01:17:49 +0300

    Make sure extra parameters on destination interface are ignored when adapting from static methods

---
+++ AutoAdapter.Tests.AssemblyToProcess/StaticMethodToInterfaceTests/ExtraParameterOnTargetInterfaceTest/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.StaticMethodToInterfaceTests.ExtraParameterOnTargetInterfaceTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TDestination>(Type @class, string methodName)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<IDestinationInterface>(typeof(SourceStaticClass), nameof(SourceStaticClass.Echo));
+
+            int extraParameter = 1;
+
+            adapter.Echo("value", extraParameter).Should().Be("value");
+        }
+    }
+
+    public interface IDestinationInterface
+    {
+        string Echo(string value, int extraParameter);
+    }
+
+    public static class SourceStaticClass
+    {
+        public static string Echo(string value)
+        {
+            return value;
+        }
+    }
+}

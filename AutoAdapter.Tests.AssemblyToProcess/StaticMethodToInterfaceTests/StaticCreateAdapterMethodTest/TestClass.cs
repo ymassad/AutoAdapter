349c91c5f9a31170ab3ded4b6012221d3e5a9210
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-24 23:44:25 +0300

    Making sure the CreateAdapter method used to adapt static methods to interfaces can be static

---
+++ AutoAdapter.Tests.AssemblyToProcess/StaticMethodToInterfaceTests/StaticCreateAdapterMethodTest/TestClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.StaticMethodToInterfaceTests.StaticCreateAdapterMethodTest
+{
+    public static class TestClass
+    {
+        //[AdapterMethod]
+        public static TDestination CreateAdapter<TDestination>(Type @class, string methodName)
+        {
+            throw new Exception();
+        }
+
+        public static void RunTest()
+        {
+            var adapter = CreateAdapter<IDestinationInterface>(typeof(SourceStaticClass), nameof(SourceStaticClass.Echo));
+
+            adapter.Echo("Input").Should().Be("Input");
+        }
+    }
+
+
+    public interface IDestinationInterface
+    {
+        string Echo(string value);
+    }
+
+    public static class SourceStaticClass
+    {
+        public static string Echo(string value)
+        {
+            return value;
+        }
+    }
+}

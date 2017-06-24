61fd94ff9ee4bec6f00dfa2fa00e4aa32167463d
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-25 00:15:03 +0300

    Making sure we can handle adapting from a static method in a non-static class

---
+++ AutoAdapter.Tests.AssemblyToProcess/StaticMethodToInterfaceTests/StaticMethodInNonStaticClassTest/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.StaticMethodToInterfaceTests.StaticMethodInNonStaticClassTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TDestination>(Type @class, string methodName)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<IDestinationInterface>(typeof(SourceClass), nameof(SourceClass.Echo));
+
+            adapter.Echo("Input").Should().Be("Input");
+        }
+    }
+
+    public interface IDestinationInterface
+    {
+        string Echo(string value);
+    }
+
+    public class SourceClass
+    {
+        public static string Echo(string value)
+        {
+            return value;
+        }
+    }
+}

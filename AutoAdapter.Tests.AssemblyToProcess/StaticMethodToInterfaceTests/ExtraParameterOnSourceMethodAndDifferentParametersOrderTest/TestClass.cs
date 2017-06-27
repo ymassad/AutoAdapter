327e62257cff0e1b6866a7f8c71b83dc2101832d
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-27 23:52:57 +0300

    Making sure we support the case of extra parameter on the source static method that is at the beginning of the parameter list

---
+++ AutoAdapter.Tests.AssemblyToProcess/StaticMethodToInterfaceTests/ExtraParameterOnSourceMethodAndDifferentParametersOrderTest/TestClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.StaticMethodToInterfaceTests.ExtraParameterOnSourceMethodAndDifferentParametersOrderTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TDestination>(object extraParameters, Type @class, string methodName)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<IDestinationInterface>(
+                new { extraParameter = 1 },
+                typeof(SourceStaticClass),
+                nameof(SourceStaticClass.Concat));
+
+            adapter.Concat("Input").Should().Be("Input1");
+        }
+    }
+
+    public interface IDestinationInterface
+    {
+        string Concat(string value);
+    }
+
+    public static class SourceStaticClass
+    {
+        public static string Concat(int extraParameter, string value)
+        {
+            return value + extraParameter;
+        }
+    }
+}

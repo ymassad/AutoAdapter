940d345287c0f5617b6a3b016462bd6ea4b521b4
Yacoub Massad <yacoubmassad@gmail.com>
2017-06-27 23:58:00 +0300

    When doing static method to interface adaptation: making sure that if we supply two different values for an extra parameter at the source methods in two different adaptions requests, all works fine.

---
+++ AutoAdapter.Tests.AssemblyToProcess/StaticMethodToInterfaceTests/ExtraParameterOnSourceMethod_TwoRequestsEachWithDifferentExtraParameterValueTest/TestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.StaticMethodToInterfaceTests.ExtraParameterOnSourceMethod_TwoRequestsEachWithDifferentExtraParameterValueTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TDestination>(object extraParameters, Type @class, string methodName)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<IDestinationInterface>(
+                new { extraParameter = 1 },
+                typeof(SourceStaticClass),
+                nameof(SourceStaticClass.Concat));
+
+            adapter.Concat("Input").Should().Be("Input1");
+
+            var adapter2 = CreateAdapter<IDestinationInterface>(
+                new { extraParameter = 2 },
+                typeof(SourceStaticClass),
+                nameof(SourceStaticClass.Concat));
+
+            adapter2.Concat("Input").Should().Be("Input2");
+        }
+    }
+
+    public interface IDestinationInterface
+    {
+        string Concat(string value);
+    }
+
+    public static class SourceStaticClass
+    {
+        public static string Concat(string value, int extraParameter)
+        {
+            return value + extraParameter;
+        }
+    }
+}

53bf59b4e0780d93f15778825b6f052d24800d17
Yacoub Massad <yacoubmassad@gmail.com>
2017-08-14 22:21:50 +0300

    Making sure we support the case of adapting a non-generic interface to a generic interface

---
+++ AutoAdapter.Tests.AssemblyToProcess/InterfaceToInterfaceTests/NonGenericInterfaceToGenericInterfaceTest/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+
+namespace AutoAdapter.Tests.AssemblyToProcess.InterfaceToInterfaceTests.NonGenericInterfaceToGenericInterfaceTest
+{
+    public class TestClass
+    {
+        //[AdapterMethod]
+        public TDestination CreateAdapter<TSource, TDestination>(TSource instance)
+        {
+            throw new Exception();
+        }
+
+        public void RunTest()
+        {
+            var adapter = CreateAdapter<ISourceInterface, IDestinationInterface<string>>(new SourceClass());
+
+            adapter.Echo("Input").Should().Be("Input");
+        }
+    }
+
+    public interface ISourceInterface
+    {
+        string Echo(string value);
+    }
+
+    public interface IDestinationInterface<T>
+    {
+        string Echo(T value);
+    }
+
+    public class SourceClass : ISourceInterface
+    {
+        public string Echo(string value)
+        {
+            return value;
+        }
+    }
+}
